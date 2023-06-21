087c1611b4868ba81072da336a7d3b547055460f
Maulik <maxmehrotra@gmail.com>
2023-06-21 16:58:56 +0530

    functions

---
+++ polygon.py
@@ -0,0 +1,26 @@
+from turtle import*
+def hexagon(d):            #function main body
+    for i in range(6):
+        fd(d)
+        lt(60)
+def pentagon():           #function main body
+    for i in range(5):
+      fd(100)
+      lt(72)
+def triangle():
+    for i in range(3):
+        fd(100)
+        lt(60)
+def square():
+    for i in range(4):
+        fd(100)
+        lt(90)
+def septagon():
+    for i in range(7):
+        fd(100)
+        lt(51.4)
+def octagon():
+    for i in range(8):
+
+        fd(100)
+        lt(45)
